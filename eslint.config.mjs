fb88629867e4e47b81cba66a5fd577de962bfc1d
rowan-maynor <rowanmaynor@gmail.com>
2025-02-07 11:52:28 -0500

    fix: Allow eslint to recognize jest globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-
+import jestPlugin from "eslint-plugin-jest";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  jestPlugin.configs.recommended,
 ];
